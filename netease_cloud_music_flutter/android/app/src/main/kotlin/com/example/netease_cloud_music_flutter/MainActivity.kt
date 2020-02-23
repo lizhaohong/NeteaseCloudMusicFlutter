9a0ca00ef265217d1d0918db3d89d40b12c85514
昭宏李 <l.zh19@mail.scut.edu.cn>
2020-02-23 14:07:14 +0800

    flutter工程创建完毕，计划在到公司开工前剩下的十多天里面，认真完成网易云音乐flutter版的开发，好好熟悉flutter

---
+++ netease_cloud_music_flutter/android/app/src/main/kotlin/com/example/netease_cloud_music_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.netease_cloud_music_flutter
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
